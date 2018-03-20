48b3bdcebec140e13899bd856d192568a26751e0
crypto-banana <cryptoshuri@gmail.com>
2018-03-19 22:38:58 -0500

    Putting initial timeout scenarios in place

--- contracts/TTTChannel.sol
+++ contracts/TTTChannel.sol
@@ -3,21 +3,37 @@ pragma solidity ^0.4.21;
 import 'zeppelin-solidity/contracts/ECRecovery.sol';
 
 
-// TODO: Handle timeout/un-repsonsiveness
-// NOTE: sample code at https://github.com/ethereum/EIPs/pull/712
 contract TTTChannel {
 
     using ECRecovery for bytes32;
 
     event ChallengeMade(address indexed _challengerIdx, address indexed _opponentIdx, address _challenger, address _opponent, uint256 _gameId);
 
+    uint256 public constant TIMEOUT = 10 minutes;
+
     struct Game {
         address challenger;
         address challenged;
         uint256 startTime;
         address winner;
+        uint256 timeout;
+        address inactive;
+        address waiting;
         uint8[9] moves;
     }
+
+    // The moves array is the sequence of moves in the game. Each move is
+    // represented by a number between 1-9, inclusive. Each of those numbers
+    // represents a location on the board:
+    //
+    // 1 | 2 | 3
+    // ---------
+    // 4 | 5 | 6
+    // ---------
+    // 7 | 8 | 9
+    //
+    // The challenged player moves first. So this array represents a challenger
+    // victory: [7, 1, 4, 2, 5, 3]
 
     Game[] public games;
 
@@ -25,9 +41,10 @@ contract TTTChannel {
         // TODO: Players can only play each other once per day
         // require(once per day)
         emit ChallengeMade(msg.sender, _opponent, msg.sender, _opponent, games.length);
-        games.push(Game(msg.sender, _opponent, now, 0x0, [0, 0, 0, 0, 0, 0, 0, 0, 0]));
+        games.push(Game(msg.sender, _opponent, now, 0x0, 0, 0x0, 0x0, [0, 0, 0, 0, 0, 0, 0, 0, 0]));
     }
 
+    // TODO: validate moves... validate winner?
     function gameOver(uint256 _gameId, uint8[9] _moves, address _winner, bytes _gameSig, bytes _gameOverSig) external {
         Game storage _game = games[_gameId];
 
@@ -38,6 +55,51 @@ contract TTTChannel {
 
         _game.winner = _winner;
         _game.moves = _moves;
+
+        // emit Event
+    }
+
+    function timeout(uint256 _gameId, uint8[9] _moves) external {
+        Game storage _game = games[_gameId];
+        require(_game.timeout == 0); // not already timing out
+
+        if (msg.sender == _game.challenger) {
+            _game.inactive = _game.challenged;
+            _game.waiting = _game.challenger;
+        } else if (msg.sender == _game.challenged) {
+            _game.inactive = _game.challenger;
+            _game.waiting = _game.challenged;
+        } else {
+            revert(); // only the players can call timeout
+        }
+
+        _game.moves = _moves; // validate moves
+        _game.timeout = now + TIMEOUT;
+
+        // emit Event so the other player can be notified
+    }
+
+    function cancelTimeout(uint256 _gameId, uint8[9] _moves) external {
+        Game storage _game = games[_gameId];
+        require(_game.inactive == msg.sender);
+        require(_game.timeout > now);
+
+        _game.moves = _moves; // validate moves
+        _game.timeout = 0;
+        _game.inactive = 0x0;
+        _game.waiting = 0x0;
+
+        // emit Event so the other player can be notified
+    }
+
+    function winByForfeit(uint256 _gameId) external {
+        Game storage _game = games[_gameId];
+        require(_game.waiting == msg.sender);
+        require(_game.timeout < now);
+
+        _game.winner = msg.sender;
+
+        // emit Event
     }
 
     function getSignerOfGameHash(uint256 _gameId, uint8[9] _moves, address _winner, bytes _gameSigned) public view returns (address) {
@@ -55,5 +117,9 @@ contract TTTChannel {
 
     function gamesLength() external view returns (uint256) {
         return games.length;
+    }
+
+    function gameMoves(uint256 _gameId) external view returns (uint8[9]) {
+        return games[_gameId].moves;
     }
 }
--- test/ttt_channel.js
+++ test/ttt_channel.js
@@ -1,22 +1,23 @@
 var ethUtils = require('ethereumjs-util');
 
-
 let TTTChannel = artifacts.require('TTTChannel.sol');
 let ECRecovery = artifacts.require('ECRecovery.sol');
 
+let tenMinutes = 10*60;
 
 contract('TTTChannel', function([_, challenger, challenged]) {
     let channel;
 
-    // These private keys should match the ones used by testrpc    
     let privateKeys = {};
-    privateKeys[challenger] = '0x0bbd25f3721c8efc84fb9b657ea46539163f5ef185c952ad64864c1fc8cddf8d';
-    privateKeys[challenged] = '0x80b938c0b44ed7fd8de077d82f3e6483fff826cc5c34f7c9696578a6248d52d0';
+    // These need to be changed every time ganache is restarted. They are the 2nd and 3rd private keys.
+    privateKeys[challenger] = '0xc2acccc51ed58ba9326f5b0bd0247a461ddff9e782f7e8d40c1bf6c635e62586';
+    privateKeys[challenged] = '0xf8693d08cf31c71244865d48360d4b0239a7cc798c49ac514f060fa64e6ab5da';
 
     beforeEach(async () => {
         const ecrecovery = await ECRecovery.new();
         TTTChannel.link('ECRecovery', ecrecovery.address);
         channel = await TTTChannel.new();
+        await channel.challenge(challenged, {from: challenger});
     });
 
     // Sign a message with a private key, it returns the signature in rpc format
@@ -26,21 +27,16 @@ contract('TTTChannel', function([_, challenger, challenged]) {
     }
 
     it("should create a new game when a new challenge appears", async () => {
-        await channel.challenge(challenged, {from: challenger});
         let gameCount = await channel.gamesLength();
         assert.equal(gameCount.toNumber(), 1);
     });
 
     it("should complete a game when both players sign off on it", async () => {
-        await channel.challenge(challenged, {from: challenger});
-        let moves = [1, 4, 2, 5, 3];
-
+        let moves = [7, 1, 4, 2, 5, 3]; // challenger wins
         let hash = await channel.generateGameHash(0, moves, challenger);
         let gameSig = signMsg(hash, privateKeys[challenger])
 
-        assert.equal(challenger,
-              await channel.getSignerOfGameHash(0, moves, challenger, gameSig)
-        );
+        assert.equal(await channel.getSignerOfGameHash(0, moves, challenger, gameSig), challenger);
 
         const gameHash = await channel.generateKeccak256(gameSig);
         const gameOverSig = signMsg(gameHash, privateKeys[challenged]);
@@ -50,11 +46,73 @@ contract('TTTChannel', function([_, challenger, challenged]) {
         let game = await channel.games(0);
         assert.equal(game[0], challenger);
         assert.equal(game[1], challenged);
-        assert.equal(game[3], challenger);
+        assert.equal(game[3], challenger); // winner
 
-        // TODO: How to get the moves? WTF?
-        // let firstMove = await channel.games(0, 0);
-        // assert.equal(firstMove, moves[0]);
+        let gameMoves = await channel.gameMoves(0);
+        assert.equal(gameMoves[0], 7);
+        assert.equal(gameMoves[1], 1);
     });
 
+    it("should allow game to be put in timeout by one player and then continued by other player", async () => {
+        let moves = [1]; // challenger never moves
+
+        await channel.timeout(0, moves, {from: challenged});
+
+        let game = await channel.games(0);
+        assert.equal(game[4], now() + tenMinutes); // timeout
+        assert.equal(game[5], challenger); // inactive player
+        assert.equal(game[6], challenged); // waiting player
+
+        let gameMoves = await channel.gameMoves(0);
+        assert.equal(1, gameMoves[0]);
+
+        await channel.cancelTimeout(0, [1, 2], {from: challenger});
+        game = await channel.games(0);
+        assert.equal(game[4], 0); // timeout
+        assert.equal(game[5], 0); // inactive player
+        assert.equal(game[6], 0); // inactive player
+
+        gameMoves = await channel.gameMoves(0);
+        assert.equal(2, gameMoves[1]);
+    });
+
+    it("should allow game to be put in timeout and then eventually closed out", async () => {
+        let moves = []; // challenged never moves
+
+        await channel.timeout(0, moves, {from: challenger});
+
+        increaseTime(tenMinutes+1);
+
+        await channel.winByForfeit(0, {from: challenger});
+
+        let game = await channel.games(0);
+        assert.equal(game[3], challenger); // winner
+    });
 });
+
+function now() {
+    return web3.eth.getBlock('latest').timestamp;
+}
+
+function increaseTime(duration) {
+  const id = Date.now();
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync({
+      jsonrpc: '2.0',
+      method: 'evm_increaseTime',
+      params: [duration],
+      id: id,
+    }, err1 => {
+      if (err1) return reject(err1);
+
+      web3.currentProvider.sendAsync({
+        jsonrpc: '2.0',
+        method: 'evm_mine',
+        id: id + 1,
+      }, (err2, res) => {
+        return err2 ? reject(err2) : resolve(res);
+      });
+    });
+  });
+}
